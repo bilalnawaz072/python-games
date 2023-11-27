9e86931eeb296341ebe930bda32d32474babe4bc
Muhammad Bilal <bilalnawaz072@gmail.com>
2023-11-27 11:58:11 +0500

    Implement different difficulty levels

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import random
 
-def get_computer_choice():
-    return random.choice(["rock", "paper", "scissors"])
+def get_computer_choice(difficulty_level):
+    if difficulty_level == "easy":
+        return random.choice(["rock", "paper", "scissors"])
+    elif difficulty_level == "medium":
+        weighted_choices = ["rock"] * 5 + ["paper"] * 3 + ["scissors"] * 2
+        return random.choice(weighted_choices)
+    elif difficulty_level == "hard":
+        weighted_choices = ["rock"] * 2 + ["paper"] * 3 + ["scissors"] * 5
+        return random.choice(weighted_choices)
+    else:  # difficulty level is expert
+        weighted_choices = ["rock", "paper", "scissors"]
+        return random.choices(weighted_choices, weights = [3, 4, 3])[0]
 
 def determine_winner(player_choice, computer_choice):
     if player_choice == computer_choice:
@@ -13,41 +23,43 @@ def determine_winner(player_choice, computer_choice):
     else:
         return "Computer wins!"
 
-def play_game():
+def play_game(difficulty_level):
     player_score = 0
     computer_score = 0
     tie_counter = 0
+    score_increment = {"easy": 5, "medium": 10, "hard": 15, "expert": 20}
 
     while True:
         player_choice = input("Enter rock, paper, or scissors (or 'quit' to exit): ").lower()
-        
+
         if player_choice == 'quit':
             break
-        
-        computer_choice = get_computer_choice()
+
+        computer_choice = get_computer_choice(difficulty_level)
         print(f"Computer chose {computer_choice}")
         result = determine_winner(player_choice, computer_choice)
         print(result)
-        
+
         if result == "Player wins!":
-            player_score += 10
+            player_score += score_increment[difficulty_level]
             print(f"Current scores: Player : {player_score}, Computer : {computer_score}")
         elif result == "Computer wins!":
-            computer_score += 10
+            computer_score += score_increment[difficulty_level]
             print(f"Current scores: Player : {player_score}, Computer : {computer_score}")
         elif result == "It's a tie!":
             tie_counter += 1
-            player_score += 5
-            computer_score += 5
+            player_score += score_increment[difficulty_level]
+            computer_score += score_increment[difficulty_level]
             print(f"Current scores: Player : {player_score}, Computer : {computer_score}")
-        
+
         if tie_counter == 3:
             print("It has been a tie 3 times.Quitting game...")
             break
-    
+
     print("Game over!")
     print(f"Player score: {player_score}")
     print(f"Computer score: {computer_score}")
 
 if __name__ == "__main__":
-    play_game()
+    difficulty_level = input("Choose your difficulty level (easy/medium/hard/expert): ")
+    play_game(difficulty_level)
